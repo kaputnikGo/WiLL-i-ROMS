e4e66e004ad017e6bca783a25f87bcbf791e706b
kaputnikGo <10411004+kaputnikGo@users.noreply.github.com>
2021-03-14 10:52:12 +1100

    PIA2 fix
    init refactor and extended addr use

--- Synth2.asm
+++ Synth2.asm
@@ -1,4 +1,4 @@
-; SYNTH2 CODE - 13 Mar 2021
+; SYNTH2 CODE - 14 Mar 2021
 ; hack for Heathkit ET-3400 Audio Setup
 ; user RAM = 197 + 256 bytes = 453
 ; addr 0000 - 00C4 and 0100 - 01FF
@@ -6,7 +6,8 @@
 ; added PIA2 addr 4000
 ; mpu clock speed is default/low (quoted as 0.5 MHz), expecting ~894750 cycles per second
 ; working, looping one shot with ramp up/down, pitch/frequency and duration params
-; still in progress
+; INIT and PIA2 fix 
+;
 ;*************************************;
 ; Main loop scratch memory reserves
 ;*************************************;
@@ -24,19 +25,19 @@
 ;*************************************;
 0011 : 8E 01 FF   lds #$01FF          ; load SP with 01FFh
 0014 : CE 80 00	  ldx #$8000          ; load X with 8000h, PIA1 (DAC) addr
-0017 : 6F 01		  clr	$01,x           ; clear(00) addr X + 01h (8001 PIA1 DDR port A) 
-0019 : 6F 03      clr $03,x           ; clear(00) addr X + 03h (8003 PIA1 DDR port B)
-001B : 6F 02      clr $02,x           ; clear(00) addr X + 02h (8002 PIA1 port B in)
-001D : 86 FF		  ldaa	#$FF          ; load A with FFh (1111 1111)
-001F : A7 00		  staa	$00,x         ; store A in addr X + 00h (8000 PIA1 port A out)
-0021 : 86 3C		  ldaa	#$3C          ; load A with 3Ch(0011 1100)
-0023 : A7 01		  staa	$01,x         ; store A in addr X + 01h (8001 PIA1 port A)
-0025 : 86 37      ldaa  #$37          ; load A with 37h(0011 0111)
-0027 : A7 03      staa  $03,x         ; store A in addr X + 03h (8003 PIA1 port B) 
-0029 : CE 40 00   ldx #$4000          ; load X with 4000h, PIA2 addr
-002C : 6F 03      clr $03,x           ; clear(00) addr X + 03h (4003 PIA2 DDR port B)
-002E : 6F 02      clr $02,x           ; clear(00) addr X + 02h (4002 PIA2 port B in)
-0030 : 01 01      nop nop             ; (staa  $03,x) store A in addr X + 03h (4003 PIA2 port B)
+0017 : 6F 02      clr $02,x           ; clear(00) addr X + 02h (set 8002 PIA1 PR/DDR port B in)
+0019 : 86 FF		  ldaa	#$FF          ; load A with FFh (1111 1111)
+001B : A7 00		  staa	$00,x         ; store A in addr X + 00h (set 8000 PIA1 PR/DDR port A out)
+001D : 86 3C		  ldaa	#$3C          ; load A with 3Ch(0011 1100)
+001F : A7 01		  staa	$01,x         ; store A in addr X + 01h (8001 PIA1 CR port A)
+0021 : 86 37      ldaa  #$37          ; load A with 37h(0011 0111)
+0023 : A7 03      staa  $03,x         ; store A in addr X + 03h (8003 PIA1 CR port B) 
+0025 : 7F 40 02   clr X4002           ; clear(00) 4002h (set PIA2 PR/DDR port B in)
+0028 : 86 04      ldaa  #$04          ; set CR bit 2 high for PIA2
+002A : B7 40 03   staa X4003          ; store A in addr 4003 (PIA2 CR port B)
+002D : 01         nop                 ;
+; ~ all nops here - SPARE
+0031 : 01         nop                 ; 
 ;*************************************;
 ;IRQ main loop start
 ;*************************************;
